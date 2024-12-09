24c373793e0709bf5e7ec1bc99537d61e64156d6
laurentiu-bereanu <laurentiu.bereanu97@gmail.com>
2024-12-09 17:08:24 +0200

    refactor and cleanup

--- AutomationProject_NET/AutomationFramework/Factory/DriverFactory.cs
+++ AutomationProject_NET/AutomationFramework/Factory/DriverFactory.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using AutomationProject_NET.AutomationFramework.Factory.Manager;
+﻿using AutomationProject_NET.AutomationFramework.Factory.Manager;
 using OpenQA.Selenium;
 
 namespace AutomationProject_NET.AutomationFramework.Factory
